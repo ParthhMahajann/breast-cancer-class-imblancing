7b6815e654acf7abfa9731013986338a2ef0510e
ParthhMahajann <parthmahajan740@gmail.com>
2025-10-24 09:13:01 +0530

    data processed and baseline-model created

---
+++ src/data/processed/view-npy.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Load the .npy file (change path as needed)
+data = np.load('X_test_scaled.npy')
+
+# Print the data (or its shape, type, etc.)
+print(data)
+print("Shape:", data.shape)
+print("Data type:", data.dtype)
